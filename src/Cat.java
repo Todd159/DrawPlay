950504f3d1791f9b161dd2be36b28e1527e644c8
Todd Davis <davis1@mines.edu>
2021-09-30 12:02:43 -0600

    Stealth Cat -Todd

--- src/Cat.java
+++ src/Cat.java
@@ -9,15 +9,15 @@ public class Cat {
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/4;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 2;
+	private static final int EYE_HEIGHT = 4;
 	private static final int EYE_WIDTH = 20;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 30;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
+	private static final int MOUTH_HEIGHT = 20;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_X = HEAD_DIMENSION/6 * 3;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
 	// draw will render the Cat on the Graphics object
@@ -27,23 +27,23 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.orange);
+		g2.setColor(Color.BLACK);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.red);
+		g2.setColor(Color.white);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.black);
+		g2.setColor(Color.DARK_GRAY);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Ughhh", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Shhh", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
